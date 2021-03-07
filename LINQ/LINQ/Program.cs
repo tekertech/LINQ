b63288c93d17716336feb0117e5e92b7d55334f5
Mehmet Teker <teker.mehmed@gmail.com>
2021-03-07 17:45:18 +0300

    using orderby, ThenBy, OrderByDescending, ThenByDescending

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -10,42 +10,37 @@ namespace LINQ
         public string Name { get; set; }
         public int Rank { get; set; }
 
+        public int Age { get; set; }
+
     }
     class Program
     {
         static void Main(string[] args)
         {
             List<Student> students = new List<Student>();
-            students.Add(new Student() { ID = 1, Name = "Ali", Rank = 10});
-            students.Add(new Student() { ID = 2, Name = "Ayşe", Rank = 11 });
-            students.Add(new Student() { ID = 3, Name = "Furkan", Rank = 12 });
-            students.Add(new Student() { ID = 4, Name = "Ferhat", Rank = 13 });
+            students.Add(new Student() { ID = 1, Name = "Ali",    Rank = 1 , Age = 11});
+            students.Add(new Student() { ID = 2, Name = "Ayşe",   Rank = 1 , Age = 12});
+            students.Add(new Student() { ID = 3, Name = "Furkan", Rank = 2 , Age = 10});
+            students.Add(new Student() { ID = 4, Name = "Ferhat", Rank = 2 , Age = 5});
 
             // Query Syntax
-            var resultStudent = from student in students where student.Name.Contains("F") orderby student.Rank descending select student;
+            var resultQuery = from student in students orderby student.Rank, student.Age select student;
 
-            foreach (var item in resultStudent)
+            foreach (var item in resultQuery)
             {
-                Console.WriteLine(" Name : " + item.Name);
+                Console.WriteLine("Query : " + item.Name);
             }
+
 
             // Method Syntax
+            var resultMethod = students.Where(x => x.ID > 2 && x.Name.Contains("F")).OrderByDescending(x => x.Age).ThenByDescending(x => x.ID);
 
 
-            var result = students.OrderBy(x => x.Rank);
 
-            foreach (var item in result)
+            foreach (var item in resultMethod)
             {
-                Console.WriteLine("Order By ASC:" + item.Name);
+                Console.WriteLine("Method : " + item.Name);
             }
-
-            var resultDESC = students.OrderByDescending(x => x.Rank);
-
-            foreach (var item in resultDESC)
-            {
-                Console.WriteLine("Order By Desc : " + item.Name);
-            }
-
 
             Console.ReadLine();
         }
