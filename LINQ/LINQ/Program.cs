43d0679ea7fdc97a938a46e9f3f71e24f7b2abd5
Mehmet Teker <teker.mehmed@gmail.com>
2021-03-07 18:41:26 +0300

    Using Reverse, Except

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -4,42 +4,41 @@ using System.Linq;
 
 namespace LINQ
 {
-    class Student 
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public int Rank { get; set; }
-
-        public int Age { get; set; }
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            List<Student> students = new List<Student>();
-            students.Add(new Student() { ID = 1, Name = "Ali",    Rank = 1 , Age = 11});
-            students.Add(new Student() { ID = 2, Name = "Ayşe",   Rank = 1 , Age = 12});
-            students.Add(new Student() { ID = 3, Name = "Furkan", Rank = 2 , Age = 10});
-            students.Add(new Student() { ID = 4, Name = "Ferhat", Rank = 2 , Age = 5});
+            int[] intArray = new int[] { 1, 2, 3, 4, 5, 6, 7 };
 
-            // Query Syntax
-            var resultQuery = from student in students orderby student.Rank, student.Age select student;
-
-            foreach (var item in resultQuery)
+            foreach (var item in intArray)
             {
-                Console.WriteLine("Query : " + item.Name);
+                Console.Write(" " + item + " , ");
+            }
+
+            Console.WriteLine();
+            var reverseArray = intArray.Reverse();
+
+            foreach (var item in reverseArray)
+            {
+                Console.Write(" " + item + " , ");
             }
 
 
-            // Method Syntax
-            var resultMethod = students.Where(x => x.ID > 2 && x.Name.Contains("F")).OrderByDescending(x => x.Age).ThenByDescending(x => x.ID);
+            string[] namesDoctor = { "Ahmet", "Ali", "Ayşe", "Fatma" };
+            string[] namesEngineer = { "Ahmet", "Ali", "Furkan", "Cevdet" };
 
+            var doctorFarkEngineer = namesDoctor.Except(namesEngineer);
 
-
-            foreach (var item in resultMethod)
+            foreach (var item in doctorFarkEngineer)
             {
-                Console.WriteLine("Method : " + item.Name);
+                Console.WriteLine(item);
+            }
+
+            var engineerDiffDoctor = namesEngineer.Except(namesDoctor);
+
+            foreach (var item in engineerDiffDoctor)
+            {
+                Console.WriteLine(item);
             }
 
             Console.ReadLine();
