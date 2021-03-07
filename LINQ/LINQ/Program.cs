3411a1cd5a33cacfb0783deed4019fb265a03186
Mehmet Teker <teker.mehmed@gmail.com>
2021-03-07 11:43:13 +0300

    LINQ Query Syntax and Method Syntax

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> countries = new List<string>();
+            countries.Add("India");
+            countries.Add("USA");
+            countries.Add("UK");
+
+            
+            foreach (var item in countries)
+            {
+                Console.WriteLine("Ülke : " + item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LINQ/LINQ/ReadMe.txt
@@ -0,0 +1,10 @@
+﻿LINQ Query Syntax and Method Syntax
+
+* Query Syntax : from Clause
+     
+     from <optional data type> <range variable> in <IEnumerable <T>>   <Query Operator> lambda expression <select> <range variable | fields of range variable>
+
+
+* Method Syntax
+
+
