aa7f7aa0814c566f5742dc83c371fab6373f0dc2
Mehmet Teker <teker.mehmed@gmail.com>
2021-03-21 19:21:42 +0300

    Range, Repeat, Zip

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -5,87 +5,39 @@ using System.Linq;
 namespace LINQ
 {
 
-
-    public class Employee
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Department { get; set; }
-
-        public static List<Employee> GetEmployees()
-        {
-            return new List<Employee>() {
-                                        new Employee(){ID = 1, Name = "A", Department = "IT"},
-                                        new Employee(){ID = 2, Name = "B", Department = "IT"},
-                                        new Employee(){ID = 3, Name = "C", Department = "IT"},
-                                        new Employee(){ID = 4, Name = "D", Department = "HR"},
-                                        new Employee(){ID = 5, Name = "E", Department = "HR"},
-                                        new Employee(){ID = 6, Name = "F", Department = "HR"},
-                                        new Employee(){ID = 7, Name = "F", Department = "HR"},
-                                        new Employee(){ID = 8, Name = "H", Department = "HR"},
-                                        new Employee(){ID = 9, Name = "J", Department = "IT"},
-                                        new Employee(){ID = 10, Name = "K", Department = "IT"},
-                                        new Employee(){ID = 11, Name = "L", Department = "IT"},
-                                        new Employee(){ID = 12, Name = "V", Department = "IT"},
-
-                                       };
-        }
-    }
-
-
-
-
-
     class Program
     {
-        
         static void Main(string[] args)
         {
-            //Take , TakeWhile && Where
-            List<int> numbers = new List<int>() { 8, 1, 2, 3, 8, 9, 4, 5, 6, 7 };
-            var number = numbers.Take(4);
-            number = (from num in numbers select num).Take(4);
-            number = numbers.Take(4).Where(x => x > 3);
-            number = numbers.TakeWhile(x => x < 3);
-            //number = numbers.Where(x => x < 3);
-
-            // Skip, SkipWhile
-
-            number = numbers.Skip(2);
-            number = numbers.OrderBy(num => num).Where(x => x > 3).Skip(4);
-            number = numbers.Skip(4).Where(num => num < 6);
-
-            number = numbers.SkipWhile(x => x < 5);
-
-            foreach (var item in number)
+            // Range
+            List<int> numbers = Enumerable.Range(1, 10).ToList();
+            numbers = Enumerable.Range(1, 10).Where(x => x % 2 == 0).ToList();
+            numbers = Enumerable.Range(5, 7).Select(x => x * x).ToList();
+            foreach (var item in numbers)
             {
                 Console.WriteLine(item);
             }
 
-            int RecordsPerPage = 4;
-            int PageNumber = 0;
-            bool flag = true;
-            
-            do
+            // Repeat
+
+            IEnumerable<string> names = Enumerable.Repeat("Mehmet", 5);
+
+            foreach (var item in names)
             {
-                
-                Console.WriteLine("1 ile 4 arasında bir sayfa numarası giriniz");
+                Console.WriteLine(item);
+            }
 
-                if (int.TryParse(Console.ReadLine(), out PageNumber))
-                {
+            //Zip
 
-                    var result = Employee.GetEmployees().Skip((PageNumber - 1) * RecordsPerPage).Take(RecordsPerPage).ToList();
+            int[] numberSequence = { 10, 20, 30, 40 };
+            string[] nameSequence = { "A", "B", "C", "D", "E", "F", };
 
-                    foreach (var item in result)
-                    {
-                        Console.WriteLine(item.ID + " " + item.Name + " " + item.Department);
-                    }
-                }
-                else {
-                    flag = false;
-                }
+            var result = numberSequence.Zip(nameSequence, (x, y) => x + " - " + y);
 
-            } while (flag);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
 
             Console.ReadLine();
         }
